e3ff4cfe5dc6726cc170f4c77733e24fe6629473
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-10-28 00:08:56 +0600

    problem 1 solve

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1.Define a function with two parameters, assigning a default value to the second one. Call it with both one and two arguments, and show the outputs.
+
+
+
+
+let output = (a, b = 10) => {
+
+    console.log(a, b);
+
+
+}
+
+output(10, 20);
+
+
+// 2.Create a function that takes a name and a city as input and returns a multi-line message using template literals, incorporating both variables.
