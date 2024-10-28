87fd8c147950d0c50b5cbc6f33ffdd62cc437578
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-10-28 08:18:29 +0600

    problem 2 solve

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 let output = (a, b = 10) => {
 
-    console.log(a, b);
+    // console.log(a + b, a, b);
 
 
 }
@@ -13,4 +13,14 @@ let output = (a, b = 10) => {
 output(10, 20);
 
 
-// 2.Create a function that takes a name and a city as input and returns a multi-line message using template literals, incorporating both variables.
+// 2. Create a function that takes a name and a city as input and returns a multi - line message using template literals, incorporating both variables.
+
+
+let address = (name, city) => {
+
+    let addressCity = `He live in ${name} ${city} at the age of 15   `;
+
+    return addressCity;
+
+}
+console.log(address("Bangladesh", "Dhaka"));
